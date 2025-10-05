0e6aed0759967ef3a6f1f0e826c2859ae571ac07
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-05 14:07:50 +0800

    "First Code"

---
+++ prac_03/exceptions_to_complete.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 - Practical
+Fill in the TODOs to complete the task
+"""
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))
+        is_finished = True  #TODO: Set is_finished to True to exit the loop when input is valid
+    except ValueError:  #TODO: Catch only ValueError exceptions (invalid input type)
+        print("Please enter a valid integer.")
+
+print("Valid result is:", result)
