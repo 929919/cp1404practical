5a19f669123fd6762af1e09cdd32f623f455349d
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-10 00:07:43 +0800

    "creating second function "

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,3 +8,14 @@ FILENAME = "subject_data.txt"
 def main():
     data = load_data()
     display_subject_details(data)
+
+def load_data():
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
+    with open(FILENAME) as input_file:
+        for line in input_file:
+            line = line.strip()
+            parts = line.split(',')
+            parts[2] = int(parts[2])
+            data.append(parts)
+    return data
