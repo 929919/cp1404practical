56c055cf435a351f5cbd0086f18863ee379fbf8e
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-10 00:30:19 +0800

    "Display income  "

---
+++ prac_04/total_income.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    num_months = int(input("How many months? "))
+
+    for month in range(1, num_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
+
+    print_income_report(incomes, num_months)
