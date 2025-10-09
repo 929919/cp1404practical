d946d41ba823e778882fb37aea21fc715aec0d90
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-09 23:32:12 +0800

    "fixing the code "

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -2,7 +2,7 @@ def main():
     """Prompt user for 5 numbers, store in a list, and display statistics."""
     numbers = []
     for i in range(5):
-        number = int(input(f"Number: "))
+        number = int(input("Number: "))
         numbers.append(number)
 
     print(f"The first number is {numbers[0]}")
@@ -11,14 +11,18 @@ def main():
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
 
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
-username = input("Enter your username: ")
+
+def check_access():
+    """Check if the username is in the allowed list."""
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command','ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+    username = input("Enter your username: ")
     if username in usernames:
         print("Access granted")
     else:
         print("Access denied")
 
 
+check_access()
 main()
-
 
