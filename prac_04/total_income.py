6b5991b2a8518c1993389b92805b0b867bdc9e45
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-10 00:33:52 +0800

    "Print income "

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,3 +13,14 @@ def main():
         incomes.append(income)
 
     print_income_report(incomes, num_months)
+
+def print_income_report(incomes, num_months):
+    """Print income report given the incomes and number of months."""
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, num_months + 1):
+        income = incomes[month - 1]
+        total += income
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
+main()
