76a95b859cc4fe30ed4d7c6b8338740caf376420
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-09 23:06:21 +0800

    "creating a list comprehension with loop"

---
+++ prac_04/list_comprehensions.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical
+List comprehensions
+"""
+
+names = ["Bob", "Angel", "Jimi", "Alan", "Ada"]
+full_names = ["Bob Martin", "Angel Harlem", "Jimi Hendrix", "Alan Turing", "Ada Lovelace"]
+
+# for loop that creates a new list containing the first letter of each name
+first_initials = []
+for name in names:
+    first_initials.append(name[0])
+print(first_initials)
+
+# list comprehension that does the same thing as the loop above
+first_initials = [name[0] for name in names]
+print(first_initials)
