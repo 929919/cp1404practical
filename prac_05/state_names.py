d84435beaea8398ef3bff17867b5147505495b7a
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-19 20:28:08 +0800

    Add user input to look up state names by abbreviation

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,5 +1,5 @@
 """
-Program to store Australian state abbreviations and names.
+Program to look up Australian state names by abbreviation.
 """
 
 CODE_TO_NAME = {
@@ -13,4 +13,10 @@ CODE_TO_NAME = {
     "SA": "South Australia"
 }
 
-print("State dictionary loaded successfully.")
+state_code = input("Enter short state: ").strip().upper()
+while state_code:
+    if state_code in CODE_TO_NAME:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    else:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").strip().upper()
