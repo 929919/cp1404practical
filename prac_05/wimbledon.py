cb8d7ddf284db81c28d4732761ed896d9329b04d
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-19 20:59:16 +0800

    Add function to process Wimbledon data for champions and countries

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,3 +7,15 @@ def read_wimbledon_data(filename):
     """Read the Wimbledon CSV file and return data as a list of lists."""
     with open(filename, "r", encoding="utf-8-sig") as file:
         return [line.strip().split(',') for line in file.readlines()[1:]]
+
+def process_wimbledon_data(data):
+    """Count wins per champion and list winning countries."""
+    champions = {}
+    countries = set()
+
+    for entry in data:
+        champion, country = entry[2], entry[1]
+        champions[champion] = champions.get(champion, 0) + 1
+        countries.add(country)
+
+    return champions, sorted(countries)
