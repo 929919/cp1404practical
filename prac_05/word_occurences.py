095f68431be2fb46ddac2bba63092461980d33e7
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-19 21:16:17 +0800

    Add display of word occurrences sorted alphabetically and final version

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,3 +1,9 @@
+"""
+Word Occurrences
+Estimate: 15 minutes
+Actual: 9 minutes
+"""
+
 def main():
     """Count occurrences of words in a string and display results."""
     text = input("Text: ")
@@ -6,3 +12,11 @@ def main():
 
     for word in words:
         word_counts[word] = word_counts.get(word, 0) + 1
+
+    max_length = max(len(word) for word in word_counts)
+
+    for word in sorted(word_counts):
+        print(f"{word:{max_length}} : {word_counts[word]}")
+
+if __name__ == "__main__":
+    main()
