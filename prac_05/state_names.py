5fa4db9d90e53741c54e590205901c557e0ed872
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-19 20:27:47 +0800

    Add dictionary mapping state codes to full state names

---
+++ prac_05/state_names.py
@@ -0,0 +1,16 @@
+"""
+Program to store Australian state abbreviations and names.
+"""
+
+CODE_TO_NAME = {
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania",
+    "SA": "South Australia"
+}
+
+print("State dictionary loaded successfully.")
