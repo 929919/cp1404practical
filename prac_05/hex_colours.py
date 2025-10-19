6bcffd1abe9043e06905a25d48437593d5fe39b4
João Gabriel R Costa <jgcosta.estudante@gmail.com>
2025-10-19 20:15:38 +0800

    Add dictionary with sample hex colour codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,18 @@
+"""
+Program to store colour names and their hexadecimal codes.
+"""
+
+HEX_COLOURS = {
+    "AliceBlue": "#F0F8FF",
+    "AntiqueWhite": "#FAEBD7",
+    "Aqua": "#00FFFF",
+    "Aquamarine": "#7FFFD4",
+    "Azure": "#F0FFFF",
+    "Beige": "#F5F5DC",
+    "Bisque": "#FFE4C4",
+    "Black": "#000000",
+    "BlanchedAlmond": "#FFEBCD",
+    "Blue": "#0000FF"
+}
+
+print("Colour dictionary loaded successfully.")
